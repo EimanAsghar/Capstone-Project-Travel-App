b51c541bcc2c079879178bf94190ef06f63514bf
Eiman Asghar <65137832+EimanAsghar@users.noreply.github.com>
2023-02-14 14:55:07 +0300

    test

---
+++ test/performAction.spec.js
@@ -0,0 +1,8 @@
+import { performAction } from "../src/client/js/performAction"
+  
+describe("Testing the submit functionality", () => {
+
+    test("Testing the performAction() function", () => {
+      
+        expect(performAction).toBeDefined();
+    })});
